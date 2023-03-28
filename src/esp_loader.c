a75bfbacd28cee272734964a394f8ae69f1f64b1
Lennart <Lennart@binarylabs.dev>
2023-03-28 21:11:09 +0200

    fix -Wundef

--- src/esp_loader.c
+++ src/esp_loader.c
@@ -47,7 +47,7 @@ static uint32_t s_flash_write_size = 0;
 static const target_registers_t *s_reg = NULL;
 static target_chip_t s_target = ESP_UNKNOWN_CHIP;
 
-#if MD5_ENABLED
+#ifdef MD5_ENABLED
 
 static const uint32_t MD5_TIMEOUT_PER_MB = 800;
 static struct MD5Context s_md5_context;
@@ -339,7 +339,7 @@ esp_loader_error_t esp_loader_change_transmission_rate(uint32_t transmission_rat
     return loader_change_baudrate_cmd(transmission_rate);
 }
 
-#if MD5_ENABLED
+#ifdef MD5_ENABLED
 
 static void hexify(const uint8_t raw_md5[16], uint8_t hex_md5_out[32])
 {
